76d524ff1756cedef1fb3e4473ecdce099b97c99
Simon-Oliver <stauffer.simon@outlook.com>
2020-02-13 22:40:09 +0100

    init react

--- app.py
+++ app.py
@@ -5,16 +5,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-   num = make_random_num()
-   return {'number': num}
+    num = make_random_num()
+    return {'number': num}
+
+
 @app.route('/recipes')
 def recipes():
-  return {
-    'title': 'Smoked Salmon Chowder',
-    'ingredients': 'salmon, milk, bacon'
-  }
-
-
-
-
-
+    return {
+        'title': 'Smoked Salmon Chowder',
+        'ingredients': 'salmon, milk, bacon'
+    }
