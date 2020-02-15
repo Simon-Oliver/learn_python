3344b6f335416433cadc515bba577f0fe60ff0de
Simon-Oliver <stauffer.simon@outlook.com>
2020-02-15 23:29:46 +0100

    added css

--- client/src/App.js
+++ client/src/App.js
@@ -20,7 +20,9 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <h3>{this.state.temp}°C</h3>
+        <div className="Button">
+          <h3>{this.state.temp}°C</h3>
+        </div>
       </div>
     );
   }
