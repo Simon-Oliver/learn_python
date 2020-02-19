fecb68d87abd45349e5d9ce1bb8bf41ce189395b
Simon-Oliver <stauffer.simon@outlook.com>
2020-02-19 22:08:17 +0100

    added exchange rate scraping

--- app.py
+++ app.py
@@ -17,3 +17,26 @@ def recipes():
         'title': 'Smoked Salmon Chowder',
         'ingredients': 'salmon, milk, bacon'
     }
+
+
+import os
+import time
+import requests
+from bs4 import BeautifulSoup
+from xml.etree import ElementTree
+
+html = requests.get('https://www.xe.com/currencyconverter/convert/?Amount=1&From=AUD&To=CHF',
+                    headers={'User-agent': 'Mozilla/5.0'})
+xml = requests.get(
+    'http://www.pwebapps.ezv.admin.ch/apps/rates/rate/getxml?activeSearchType=today')
+soup = BeautifulSoup(html.content, "html.parser")
+num2 = soup.select_one('#main-heading')
+tree = ElementTree.fromstring(xml.content)
+print(tree)
+
+
+def read_temp():
+    dev = os.popen('/opt/vc/bin/vcgencmd measure_temp')
+    cpu_temp_s = dev.read()[5:-3]  # top and tail string
+    cpu_temp = float(cpu_temp_s)
+    return cpu_temp
--- ran_num.py
+++ ran_num.py
@@ -1,11 +1,27 @@
-import random
+import os
 import time
+import requests
+from bs4 import BeautifulSoup
+from xml.etree import ElementTree
 
-def make_random_num():
-    print(random.randrange(0, 40, 1))
-    return random.randrange(0, 40, 1)
+html = requests.get('https://www.xe.com/currencyconverter/convert/?Amount=1&From=AUD&To=CHF',
+                    headers={'User-agent': 'Mozilla/5.0'})
+xml = requests.get(
+    'http://www.pwebapps.ezv.admin.ch/apps/rates/rate/getxml?activeSearchType=today')
+soup = BeautifulSoup(html.content, "html.parser")
+num2 = soup.select_one('#main-heading')
+tree = ElementTree.fromstring(xml.content)
 
-def start_timer():
-    while True:
-        make_random_num()
-        time.sleep(1)
+for devise in tree.findall('{http://www.pwebapps.ezv.admin.ch/apps/rates}devise'):
+    name = devise.get('code')
+    kurs = devise.find(
+        '{http://www.pwebapps.ezv.admin.ch/apps/rates}kurs').text
+    if(name == 'aud'):
+        print(name, kurs)
+
+
+def read_temp():
+    dev = os.popen('/opt/vc/bin/vcgencmd measure_temp')
+    cpu_temp_s = dev.read()[5:-3]  # top and tail string
+    cpu_temp = float(cpu_temp_s)
+    return cpu_temp
