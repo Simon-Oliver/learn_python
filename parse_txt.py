532e264209220fd9ad5045c01147e362932d0c10
Simon-Oliver <stauffer.simon@outlook.com>
2020-03-03 22:28:18 +0100

    exploring dic

--- parse_txt.py
+++ parse_txt.py
@@ -3,3 +3,12 @@ f = open('sample.txt')
 for line in f:
     num = line.split()
     print(num[0])
+
+thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+
+for k, v in thisdict.items():
+    print(k, v)
