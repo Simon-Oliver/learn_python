f5f166e69a1b0e290ac1ba3399264f5a3c6a57e9
Simon-Oliver <stauffer.simon@outlook.com>
2020-03-09 22:04:05 +0100

    played with some code

--- cov_scrape.py
+++ cov_scrape.py
@@ -3,9 +3,11 @@ import csv
 import datetime
 import schedule
 
+
 class DATA:
     def __init__(self, country, date):
         pass
+
 
 today = datetime.date.today()
 date_string = today.strftime("%m-%d-%Y")
@@ -13,11 +15,12 @@ date_string = today.strftime("%m-%d-%Y")
 file_name = f"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/{date_string}.csv"
 print(file_name)
 
-# res = requests.get("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/03-07-2020.csv")
+res = requests.get(
+    "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/03-08-2020.csv")
 
-# data = csv.reader(res.text.splitlines())
+data = csv.reader(res.text.splitlines())
 
 
-# for line in data:
-#     if "Switzerland" in line:
-#         print(line)
+for line in data:
+    if "Switzerland" in line:
+        print(line)
