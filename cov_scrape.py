ba5e820ce6cfd74b809a7c4c0bc3a04ebd82c068
Simon-Oliver <stauffer.simon@outlook.com>
2020-03-10 21:23:55 +0100

    added subtracting day

--- cov_scrape.py
+++ cov_scrape.py
@@ -1,7 +1,7 @@
 import requests
 import csv
-import datetime
 import schedule
+from datetime import datetime, timedelta
 
 
 class DATA:
@@ -9,7 +9,7 @@ class DATA:
         pass
 
 
-today = datetime.date.today()
+today = datetime.today()
 date_string = today.strftime("%m-%d-%Y")
 
 file_name = f"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/{date_string}.csv"
@@ -19,7 +19,8 @@ res = requests.get(
     "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/03-08-2020.csv")
 
 data = csv.reader(res.text.splitlines())
-
+yesterday = datetime.now() + timedelta(days=-1)
+print(yesterday.strftime("%m-%d-%Y"))
 
 for line in data:
     if "Switzerland" in line:
