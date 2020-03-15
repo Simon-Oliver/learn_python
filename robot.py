35c4c4ab48d6dbaf4fc37d1797f5b7d503b274e7
Simon-Oliver <stauffer.simon@outlook.com>
2020-03-15 22:25:24 +0100

    string wiritng method

--- robot.py
+++ robot.py
@@ -11,7 +11,15 @@ class Robot:
         formatted = f"--------------{self.name}--------------"
         return formatted
 
+    def write_to_f(self, str, filename, format):
+        f = open(f"./{filename}.{format}", "a")
+        f.write(str)
+
+        # Close the file
+        f.close()
+
 
 r1 = Robot("Tom", "red", 30)
 r1.introduce_self()
 print(r1.format_str())
+r1.write_to_f(r1.format_str(), "test", "txt")
