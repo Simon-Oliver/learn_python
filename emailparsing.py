47c57d49a5fa5164301965ec3b43330eb7a72b3d
Simon-Oliver <stauffer.simon@outlook.com>
2020-03-28 16:43:57 +0100

    stuck with email parsing

--- emailparsing.py
+++ emailparsing.py
@@ -1,10 +1,19 @@
 import email
 import re
+import mailparser
+from email import policy
+from email.parser import BytesParser
 
-message = email.message_from_file(
-    open(''))
 
-s = message.find('Encoding: base64')
+msg = email.message_from_file(open('./CheapTickets.eml'))
+
+mail = mailparser.parse_from_file(msg)
+#mail = mailparser.parse_from_file(f)
+
+
 # print(line[5:line.find('.')])
+# for line in message.get_payload():
+#     print(line)
 
-print(message.find('Encoding: base64'))
+
+# print(open('./CheapTickets.eml'))
