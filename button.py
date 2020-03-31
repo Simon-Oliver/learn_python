006ba45233861b89b247bdb4d72f7b8ee48eb64a
Simon-Oliver <stauffer.simon@outlook.com>
2020-03-31 22:58:10 +0200

    exploring button

--- button.py
+++ button.py
@@ -1,13 +1,14 @@
-import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
+import RPi.GPIO as GPIO  # Import Raspberry Pi GPIO library
 import time
-GPIO.setwarnings(False) # Ignore warning for now
-GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
-GPIO.setup(12, GPIO.IN) # Set pin 10 to be an input pin
+GPIO.setwarnings(False)  # Ignore warning for now
+GPIO.setmode(GPIO.BOARD)  # Use physical pin numbering
+GPIO.setup(12, GPIO.IN)  # Set pin 10 to be an input pin
 
 count = 0
 text = ""
 setting = ["ON", "OFF", "OPTIONS"]
 selected = ""
+
 
 def button_callback(channel):
     global count
@@ -19,7 +20,8 @@ def button_callback(channel):
     print(f"{setting[count]}")
     time.sleep(0.2)
 
-#GPIO.add_event_detect(12,GPIO.RISING,callback=button_callback,bouncetime=500)
+# GPIO.add_event_detect(12,GPIO.RISING,callback=button_callback,bouncetime=500)
+
 
 try:
     while True:
@@ -27,28 +29,32 @@ try:
         print("--> Pressed <--")
         start = time.time()
         time.sleep(0.2)
-        
+
         while GPIO.input(12) == GPIO.HIGH:
             time.sleep(0.02)
-            
+
         length = time.time() - start
         print(length)
-        
+
         if length > 5:
             print("Long Press")
-            
+
         elif length > 1:
             print("Medium Press")
             selected = setting[count]
             print(f"Selected Option: {selected}")
-        
+
         else:
+            if count == 2:
+                count = 0
+            else:
+                count += 1
             print("Short Press")
             print(f"{setting[count]}")
 
-#execute this code if CTRL + C is used to kill python script
+# execute this code if CTRL + C is used to kill python script
 except KeyboardInterrupt:
-  print("You've exited the program")
-#execute code inside this block as the program exits
+    print("You've exited the program")
+# execute code inside this block as the program exits
 finally:
-  GPIO.cleanup()
+    GPIO.cleanup()
