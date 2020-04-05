ccdf8427ae30f83e9b51c340abf952dd50b70ca7
Simon-Oliver <stauffer.simon@outlook.com>
2020-04-05 20:16:09 +0200

    parsing xlsx

--- handle_ xlsx.py
+++ handle_ xlsx.py
@@ -1,9 +1,27 @@
 import pandas as pd
 
-xls = pd.ExcelFile('../../Test_Formatting.xlsx')
+xls = pd.ExcelFile('../../Test_Formatting-2.xlsx')
 df1 = pd.read_excel(xls, 'Sheet1')
 
-sheet2 = xls.parse(1)
+names = xls.sheet_names
+indices = [i for i, s in enumerate(names) if 'Overview' in s]
+
+data = {
+    'quota': [],
+    'actual': []
+}
+
+
+for i in indices:
+    df = pd.read_excel(xls, names[i])
+
+    for i in range(5):
+
+        data['quota'].append(df.iat[19+i, 2])
+        data['actual'].append(df.iat[19+i, 3])
+        #print(f'{df.iat[18+i,2]} | {df.iat[18+i,3]}')
+print(data)
+
 # Now you can list all sheets in the file
 print(xls.sheet_names)
-print(df1.iat[3, 1])
+print(indices)
