db41d58f2e973456ad73578536a568b8378bba57
Simon-Oliver <stauffer.simon@outlook.com>
2020-05-17 23:32:01 +0200

    finished delete_nth

--- codewars/delete_nth.py
+++ codewars/delete_nth.py
@@ -1,2 +1,20 @@
 def delete_nth(order, max_e):
     # code here
+    dic = {}
+    new_arr = []
+
+    for n in order:
+        if n in dic:
+            if dic[n] < max_e:
+                new_arr.append(n)
+                dic[n] += 1
+        else:
+            dic[n] = 1
+            new_arr.append(n)
+
+    return new_arr
+
+
+print(delete_nth([1, 1, 3, 3, 7, 2, 2, 2, 2], 0))
+# delete_nth([20, 37, 20, 21], 1)
+# delete_nth([1, 1, 3, 3, 7, 2, 2, 2, 2], 3)
