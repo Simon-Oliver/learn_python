affb4052b699b1188007d8d26d2a46b8216f3538
Simon-Oliver <stauffer.simon@outlook.com>
2020-05-20 22:29:41 +0200

    not yet working

--- codewars/sum_dig_pow.py
+++ codewars/sum_dig_pow.py
@@ -1,9 +1,16 @@
 def sum_dig_pow(a, b):  # range(a, b + 1) will be studied by the function
     # your code here
     num_arr = list(range(a, b + 1))
+    add_arr = []
 
     for n in num_arr:
-        print([int(i) for i in str(n)])
+        arr = [int(i) for i in str(n)]
+        for num, i in enumerate(arr, start=1):
+            add_arr.append(pow(num, i))
+            print(sum(add_arr))
+        add_arr = []
 
 
-sum_dig_pow(300, 303)
+print(sum_dig_pow(300, 303))
+
+#[1, 2, 3, 4, 5, 6, 7, 8, 9, 89]
