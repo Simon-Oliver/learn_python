f4cca822341bf3b96df529ac5fe366fee1d95259
Simon-Oliver <stauffer.simon@outlook.com>
2020-05-25 21:44:40 +0200

    aded while loop and time

--- codewars/temp_humit.py
+++ codewars/temp_humit.py
@@ -1,10 +1,15 @@
 import Adafruit_DHT
+import time
+import datetime
+
 sensor = Adafruit_DHT.DHT11
 DHT11_pin = 23
 
 humidity, temperature = Adafruit_DHT.read_retry(sensor, DHT11_pin)
-if humidity is not None and temperature is not None:
-    print(
-        'Temperature={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
-else:
-    print('Failed to get reading from the sensor. Try again!')
+while True:
+    if humidity is not None and temperature is not None:
+        print(
+            f'Temperature={temperature}°C  Humidity={humidity}%  Time={datetime.datetime.now()}')
+    else:
+        print('Failed to get reading from the sensor. Try again!')
+    time.sleep(1*60)
