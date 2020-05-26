92ad2b940acd8dc99247f61a10c91ce6c8aabc3c
Simon-Oliver <stauffer.simon@outlook.com>
2020-05-26 22:16:01 +0200

    added calc_with_func

---
+++ codewars/calc_with_func.py
@@ -0,0 +1,43 @@
+def zero(arg=False): return arg(0) if arg else 0  # your code here
+
+
+def one(arg): return 1  # your code here
+
+
+def two(): return 2  # your code here
+
+
+def three(): return 3  # your code here
+
+
+def four(): return 4  # your code here
+
+
+def five(): return 5  # your code here
+
+
+def six(): return 6  # your code here
+
+
+def seven(): return 7  # your code here
+
+
+def eight(): return 8  # your code here
+
+
+def nine(): return 9  # your code here
+
+
+def plus(): return  # your code here
+
+
+def minus(): return  # your code here
+
+
+def times(): return  # your code here
+
+
+def divided_by(): return  # your code here
+
+
+print(zero())
