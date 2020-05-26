ca951000a346854e1d005945824b0155a99dbbfe
Simon-Oliver <stauffer.simon@outlook.com>
2020-05-26 22:29:53 +0200

    added closures for multiplication

--- codewars/calc_with_func.py
+++ codewars/calc_with_func.py
@@ -1,43 +1,46 @@
 def zero(arg=False): return arg(0) if arg else 0  # your code here
 
 
-def one(arg): return 1  # your code here
+def one(arg=False): return arg(1) if arg else 1  # your code here
 
 
-def two(): return 2  # your code here
+def two(arg=False): return arg(2) if arg else 2  # your code here
 
 
-def three(): return 3  # your code here
+def three(arg=False): return 3  # your code here
 
 
-def four(): return 4  # your code here
+def four(arg=False): return 4  # your code here
 
 
-def five(): return 5  # your code here
+def five(arg=False): return 5  # your code here
 
 
-def six(): return 6  # your code here
+def six(arg=False): return 6  # your code here
 
 
-def seven(): return 7  # your code here
+def seven(arg=False): return 7  # your code here
 
 
-def eight(): return 8  # your code here
+def eight(arg=False): return 8  # your code here
 
 
-def nine(): return 9  # your code here
+def nine(arg=False): return 9  # your code here
 
 
-def plus(): return  # your code here
+def plus(func): func()  # your code here
 
 
 def minus(): return  # your code here
 
 
-def times(): return  # your code here
+def times(n):
+    def multiplier(x):
+        return n * x  # your code here
+    return multiplier
 
 
 def divided_by(): return  # your code here
 
 
-print(zero())
+print(zero(times(two())))
