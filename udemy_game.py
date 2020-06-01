c3dc8b70a747984f2fe5bff3c181c06fd4e6161a
Simon-Oliver <stauffer.simon@outlook.com>
2020-06-01 21:15:27 +0200

    python classes

--- udemy_game.py
+++ udemy_game.py
@@ -1,15 +1,25 @@
 import random
 
 playerhp = 300
-enemy_attak_l = 10
-enemy_attak_h = 60
 
-while playerhp > 0:
-    damage = random.randrange(enemy_attak_l,enemy_attak_h)
-    playerhp = playerhp - damage
+class Enemy:
+    def __init__(self, enemy_attack_l, enemy_attack_h):
+        self.enemy_attack_l = enemy_attack_l
+        self.enemy_attack_h = enemy_attack_h
 
-    if playerhp < 30:
-        print("Your health is low. You have been transfered to the next Hospital")
-        break
+    def get_attack(self):
+        print(self.enemy_attack_l)
 
-    print("You've got hit and take", damage, "damage.", "Your HP is", playerhp)
+enemy1 = Enemy(10, 60)
+
+enemy1.get_attack()
+
+# while playerhp > 0:
+#     damage = random.randrange(enemy1.enemy_attak_l, enemy1.enemy_attak_h)
+#     playerhp = playerhp - damage
+#
+#     if playerhp < 30:
+#         print("Your health is low. You have been transfered to the next Hospital")
+#         break
+#
+#     print("You've got hit and take", damage, "damage.", "Your HP is", playerhp)
