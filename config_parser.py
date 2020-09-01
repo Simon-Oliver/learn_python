f7eea595a85fe4b4a17ee4d8cc29db73028f759c
Simon-Oliver <stauffer.simon@outlook.com>
2020-09-01 17:47:18 +0200

    added commit for kwarg

--- config_parser.py
+++ config_parser.py
@@ -4,9 +4,13 @@ with open("./config.json") as f:
     data = json.load(f)
 
 
-def unpackJson(color, text, isEnabled):
+def unpackJson(color, text, isEnabled, **args):
     print(color)
     print(text)
+    try:
+        print(args['notParsed2'])
+    except Exception as e:
+        print(e)
 
 
 unpackJson(**data)
