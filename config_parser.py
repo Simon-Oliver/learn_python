5bc3b96b0107ac7d32f55feb40324131253e6ade
Simon-Oliver <stauffer.simon@outlook.com>
2020-09-04 21:51:39 +0200

    added obj

--- config_parser.py
+++ config_parser.py
@@ -4,13 +4,24 @@ with open("./config.json") as f:
     data = json.load(f)
 
 
+class Settings:
+    def __init__(self, color, text, isEnabled):
+        self.color = color
+        self.text = text
+        self.isEnabled = isEnabled
+        # **args catches extra arguments that haven't been specified in the function in a dict
+
+
 def unpackJson(color, text, isEnabled, **args):
-    print(color)
-    print(text)
+    set1 = Settings(color, text, isEnabled)
     try:
         print(args['notParsed2'])
+        return set1
     except Exception as e:
         print(e)
 
 
-unpackJson(**data)
+# passes json as a dict
+obj = unpackJson(**data)
+
+print("-----", obj.color)
