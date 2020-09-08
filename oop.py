48a1e2438d35d2a2d1e989cac41bcdb4c609eef7
Simon-Oliver <stauffer.simon@outlook.com>
2020-09-08 21:33:21 +0200

    exploring oop

--- oop.py
+++ oop.py
@@ -1,11 +1,20 @@
 class Customer:
-    def __init__(self, name, membership):
+    def __init__(self, name, membership, job):
         self.name = name
         self.membership = membership
+        self.job = job
 
-    def print_name(self):
+    def print_name():
         print(f"{self.name} {self.membership}")
 
 
-c = Customer("Oliver", "Gold")
-c.print_name()
+class Job:
+    def __init__(self, title, wage):
+        self.title = title
+        self.wage = wage
+
+
+j = Job("Chef", 50000)
+
+c = Customer("Oliver", "Gold", j)
+print(c.job.wage)
