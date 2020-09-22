297c3ef6b010a6e9869424bffc68dd2dc7325798
Simon-Oliver <stauffer.simon@outlook.com>
2020-09-22 19:54:54 +0200

    finished pangram codewars

--- codewars/pangram.py
+++ codewars/pangram.py
@@ -4,14 +4,20 @@ import re
 regex = re.compile('[^a-zA-Z]')
 
 
-def is_pangram(s):
-    newS = regex.sub('', s)
+def is_pangram(param):
+
+    newS = regex.sub('', param).strip().lower()
     check = list("abcdefghijklmnopqrstuvwxyz")
+    count = 0
 
     for l in newS:
-        print(l)
+        if l in check:
+            count += 1
+
+    if count >= len(check):
+        return True
 
     return False
 
 
-is_pangram("Test lol")
+print(is_pangram("Cwm fjord bank glyphs vext quiz"))
