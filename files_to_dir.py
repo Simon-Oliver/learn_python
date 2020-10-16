fe02c25bfa1856e9024187304890c7c188f460f5
Simon-Oliver <stauffer.simon@outlook.com>
2020-10-16 20:25:24 +0200

    os in python

--- files_to_dir.py
+++ files_to_dir.py
@@ -1,11 +1,14 @@
 import os
+import shutil
 
 count = {}
 
 for f in os.listdir("/Users/Simon/Downloads"):
+    print(f)
     extension = os.path.splitext(f)[1]
     if extension not in count:
         count[extension] = 1
+
     else:
         count[extension] += 1
 
