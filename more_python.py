bc4f78141adca80b94de6ce63216b45f9ba0ca8e
Simon Stauffer <simon.stauffer@Simon-C02TV0R8HV2Q.local>
2020-10-19 11:31:39 +0200

    more python and testing commit from second computer

---
+++ more_python.py
@@ -0,0 +1,13 @@
+class TestClass:
+    def __init__(self, name, age):
+        self.age = age
+        self.name = name
+
+    def printName(self):
+        print(self.name)
+
+
+
+max_muster = TestClass("Max", 14)
+
+max_muster.printName()
