25861798df261931de119ad88973ed7ad385a22f
Simon-Oliver <stauffer.simon@outlook.com>
2020-10-21 21:51:14 +0200

    Raising exceptions and using assert

---
+++ explore_assert.py
@@ -0,0 +1,12 @@
+# Use to detect errors that should never occur and crash your program
+test_condition = True
+assert test_condition , "Wow something went really wrong!"
+
+device = 1
+assert device in (1, 0) , "Not within range"
+
+
+# We use exceptions that are caused by invalid user input or other problems
+special_error = Exception("This is a special error!")
+
+raise special_error
