3a16cb21b253c6724fe26ac365c58e5014456e90
Simon-Oliver <stauffer.simon@outlook.com>
2020-11-20 20:03:56 +0100

    exploring pptx module

--- explorations/powerpoint.py
+++ explorations/powerpoint.py
@@ -1,3 +1,14 @@
 from pptx import Presentation
 
-pps = Presentation()
+pp = Presentation("./test_powerpoint.pptx")
+
+for slide in pp.slides:  
+    for shape in slide.shapes:  
+        if not shape.has_text_frame:  
+            continue 
+        for paragraph in shape.text_frame.paragraphs:  
+            for run in paragraph.runs:  
+                print(run.text) 
+
+
+# pp.save("Output.pptx") 
